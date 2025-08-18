24adfc45082ac52e098df77be385e2d2b4250de0
sumeer-s <22BCS50108@cuchd.in>
2025-08-18 19:41:04 +0530

    Day4

---
+++ Day4/Longest Common Prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s = "";
+        int n = strs.size();
+        if (n == 0) return s;
+        sort(strs.begin(), strs.end());
+        string first = strs[0];
+        string last = strs[n - 1];
+        for (int i = 0; i < min(first.size(), last.size()); i++) {
+            if (first[i] == last[i]) {
+                s += first[i];
+            } else {
+                break;
+            }
+        }
+        return s;
+    }
+};
